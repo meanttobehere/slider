46e2880bb5a975cfbf2604774fc87cab85b0467b
exon <exon@exon.ru>
2021-10-15 23:49:29 +0500

    slider.ts and plugin.ts merged into one file

--- src/slider/slider.ts
+++ src/slider/slider.ts
@@ -1,55 +1,38 @@
 import View from './view/view';
-import Model, { ModelData } from './model/model';
-import Presenter, { PresenterEvents } from './presenter/presenter';
+import Model from './model/model'
+import Presenter from './presenter/presenter';
+import { PresenterOptions } from './presenter/preesenterInterface';
 
-export interface SliderOptions extends Partial<ModelData>, Partial<PresenterEvents>{};
-
-export default class Slider{
-    private presenter: Presenter;
-    private view: View;
-    private model: Model;
-
-    constructor(node: JQuery, options?: SliderOptions){
-        const defaultOptions: ModelData = {
-            typeVertical: false,
-            typeRange: true,
-            displayTips: true,
-            displayProgressBar: true,
-            displayScale: true,
-            minValue: 0,
-            maxValue: 50,
-            step: 5,
-            pointerPosition: 10,
-            secondPointerPosition: 40,
-        }
-        options = {...defaultOptions, ...options};
-
-        const defaultEvetns: PresenterEvents = {
-            start: () => {},
-            slide: () => {},
-            stop: () => {},
-        }
-        options = {...defaultEvetns, ...options};
-
-        this.view = new View(node);
-        this.model = new Model(options as ModelData);
-        this.presenter = new Presenter(this.model, this.view, options as PresenterEvents);   
-    }
-
-    public getSetters(){
-        return this.presenter.getSetters();
-    }
-
-    public getGetters(){
-        return this.presenter.getGetters();
-    }
-
-    public getUpdateFunction(){
-        return (options: SliderOptions) => {
-            options = {...this.model.getData(), ...options};
-            options = {...this.presenter.getEvents(), ...options};
-            this.model.setData(options as ModelData);
-            this.presenter.setEvents(options as PresenterEvents);
-        }
+declare global {
+    interface JQuery {
+        superSlider: (options?: PresenterOptions | string, arg?: any) => JQuery;
     }
 }
+
+$.fn.superSlider = function(options?: PresenterOptions | string, arg?: any) : JQuery
+{
+    if (typeof options === "object" || options === undefined){
+        let sliderIsInitialized = this.data("update");        
+        if (sliderIsInitialized){
+            this.data("update")(options as PresenterOptions);            
+            return;         
+        }
+
+        const view = new View(this);
+        const model = new Model();
+        const presenter = new Presenter(model, view, options as PresenterOptions);
+        
+        this.data("update", presenter.getUpdateFunction());
+        this.data("setters", presenter.getSetters());
+        this.data("getters", presenter.getGetters());
+    }
+
+    if (typeof options === "string"){
+        if (arg != undefined)
+            return this.data("setters")[options](arg);
+        else 
+            return this.data("getters")[options]();
+    }
+
+    return this;
+} 
