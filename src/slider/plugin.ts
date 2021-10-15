83a69bd88607f27a31c0bc553979ccb3a38bed3c
exon <exon@exon.ru>
2021-10-15 15:00:15 +0500

    plugin interface update

--- src/slider/plugin.ts
+++ src/slider/plugin.ts
@@ -1,14 +1,33 @@
-import Slider from './slider';
+import Slider, { SliderOptions } from './slider';
 
 declare global {
     interface JQuery {
-        superSlider(): void;
+        superSlider: (options?: SliderOptions | string, arg?: any) => JQuery;
     }
 }
 
-$.fn.superSlider = function()
+$.fn.superSlider = function(options?: SliderOptions | string, arg?: any) : JQuery
 {
-    let slider = new Slider(this);
+    if (typeof options === "object" || !options){        
+        const slider = new Slider(this, options as SliderOptions);
+
+        if (this.data("inizialized") == true){
+            slider.update(options as SliderOptions);           
+        } else{
+            this.data("inizialized", true);
+            this.data("setters", slider.getSetters());
+            this.data("getters", slider.getGetters());
+        }
+    }
+
+    if (typeof options === "string"){
+        if (arg)
+            return this.data("setters")[options](arg);
+        else 
+            return this.data("getters")[options]();
+    }
+
+    return this;
 }
 
 
