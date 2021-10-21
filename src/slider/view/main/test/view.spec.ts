bf53683331e857000fd8606274ac7e61e939bcea
Andromeda <andromeda@MacBook-Air-Andromeda.local>
2021-10-21 23:21:37 +0500

    update

--- src/slider/view/main/test/view.spec.ts
+++ src/slider/view/main/test/view.spec.ts
@@ -1,5 +1,22 @@
-describe("View", () => {
-    it("", () => {
+import View from '../view';
 
-    });
+describe('View', () => {
+  let $parent: JQuery;
+  let view: View;
+  let $view: JQuery;
+  let $bar;
+  let $pointer;
+  let $secondPointer;
+  let $tip;
+  let $secondTip;
+
+  beforeEach(() => {
+    $parent = $('<div>', { class: 'parent' });
+    view = new View($parent);
+    $view = $parent.children().first();
+  });
+
+  it("constructor should create element $view on parent node with class 'slider__container'", () => {
+    expect($view).toHaveClass('slider__container');
+  });
 });
