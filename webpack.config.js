d739daa1676a8774046d9874d7603b8fcdb5b92d
exon <exon@exon.ru>
2021-10-24 13:31:05 +0500

    favicon added, webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,70 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        slider: { 
-            import: './src/plugin/slider.ts', 
-            filename: 'lib/[name].js',
-            library: {                
-                name: 'superSlider',
-                type: 'umd',
-                umdNamedDefine: true,
-            },
-        },
-        demopage: { 
-            import: './example/index.ts', 
-            filename: 'demo/index.js', 
-        },             
+  entry: {
+    slider: {
+      import: './src/plugin/slider.ts',
+      filename: 'lib/[name].js',
+      library: {
+        name: 'superSlider',
+        type: 'umd',
+        umdNamedDefine: true,
+      },
     },
-
-    output: {
-        path: path.resolve(__dirname, 'dist'),
+    demopage: {
+      import: './example/index.ts',
+      filename: 'demo/index.js',
     },
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-            {
-                test: /\.css$/i,                
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-                include: /src/,
-            },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
 
-            {
-                test: /\.css$/i,                
-                use: ["style-loader", "css-loader"],
-                exclude: /src/,
-            }
-        ],
-    },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        include: /src/,
+      },
 
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "./example/index.html"),
-            filename: "demo/index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "lib/[name].css",
-        })
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        exclude: /src/,
+      },
     ],
-}
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './example/index.html'),
+      filename: 'demo/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'lib/[name].css',
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './example/favicon.png',
+      favicons: {
+        icons: {
+          appleStartup: false,
+        },
+      },
+    }),
+  ],
+};
