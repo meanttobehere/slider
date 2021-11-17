3202087f0e86e3091b865973cda02d9aceedff3b
exon <exon@exon.ru>
2021-11-17 17:19:04 +0500

    pointer updated

--- src/view/pointer/pointer.ts
+++ src/view/pointer/pointer.ts
@@ -1,9 +1,10 @@
-import './pointer.css';
+import MoveableObject from '../moveableObject/moveableObject';
 import {
   PointerInterface,
   PointerObserver,
   PointerProps,
 } from './pointerInterface';
+import './pointer.css';
 
 export default class Pointer implements PointerInterface {
   private $pointer: JQuery;
@@ -14,15 +15,10 @@ export default class Pointer implements PointerInterface {
 
   private isVertical: boolean;
 
-  private offsetX: number;
-
-  private offsetY: number;
-
   constructor(node: JQuery, isSecond?: boolean) {
     this.isSecond = isSecond === undefined ? false : isSecond;
     this.createDomElements(node);
-    this.initMouseEvents();
-    this.initTouchEvents();
+    this.atachEvents();
   }
 
   render(props: PointerProps) {
@@ -42,76 +38,34 @@ export default class Pointer implements PointerInterface {
     this.observer = observer;
   }
 
-  private initMouseEvents() {
-    const mouseMoveHandler = function handlePointerMouseMove(event: MouseEvent) {
-      this.handlePointerMove(event.clientX, event.clientY);
-    }.bind(this);
-
-    const mouseUpHandler = function handlePointerMouseUp() {
-      $(document).off('mousemove', mouseMoveHandler);
-      this.handlePointerEndMove();
-    }.bind(this);
-
-    const mouseDownHandler = function handlePointerMouseDown(event: MouseEvent) {
-      $(document).on('mousemove', mouseMoveHandler);
-      $(document).one('mouseup', mouseUpHandler);
-      this.handlePointerStartMove(event.offsetX, event.offsetY);
-    }.bind(this);
-
-    this.$pointer.on('mousedown', mouseDownHandler);
+  private createDomElements(node: JQuery) {
+    this.$pointer = $('<div>', { class: 'slider__pointer' });
+    node.append(this.$pointer);
   }
 
-  private initTouchEvents() {
-    const touchMoveHandler = function handlePointerTouchMove(event: TouchEvent) {
-      this.handlePointerMove(event.touches[0].clientX, event.touches[0].clientY);
-      event.preventDefault();
-      event.stopPropagation();
-    }.bind(this);
-
-    const touchEndHandler = function handlePointerTouchEnd() {
-      document.removeEventListener('touchmove', touchMoveHandler);
-      document.removeEventListener('touchend', touchEndHandler);
-      this.handlePointerEndMove();
-    }.bind(this);
-
-    const touchStartHandler = function handlePointerTouchStart(event: TouchEvent) {
-      document.addEventListener('touchmove', touchMoveHandler, { passive: false });
-      document.addEventListener('touchend', touchEndHandler);
-      this.handlePointerStartMove(0, 0);
-      event.preventDefault();
-      event.stopPropagation();
-    }.bind(this);
-
-    this.$pointer[0].addEventListener('touchstart', touchStartHandler, { passive: false });
+  private atachEvents() {
+    const moveableObject = new MoveableObject(this.$pointer);
+    moveableObject.setObserver(this.createMovableObjectObserver());
   }
 
-  private handlePointerStartMove(offsetX: number, offsetY: number) {
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
+  private createMovableObjectObserver() {
+    return {
+      startMove: this.handlePointerStartMove.bind(this),
+      move: this.handlePointerMove.bind(this),
+      endMove: this.handlePointerEndMove.bind(this),
+    };
+  }
+
+  private handlePointerStartMove() {
     this.observer?.startMove(this.isSecond);
   }
 
-  private handlePointerMove(clientX: number, clientY: number) {
-    const posX = clientX - this.offsetX;
-    const posY = clientY - this.offsetY;
-    const distance = this.calcDistanceInPercent(posX, posY);
+  private handlePointerMove(distanceX: number, distanceY: number) {
+    const distance = this.isVertical ? distanceY : distanceX;
     this.observer?.move(distance, this.isSecond);
   }
 
   private handlePointerEndMove() {
     this.observer?.endMove(this.isSecond);
-  }
-
-  private calcDistanceInPercent(posX: number, posY: number): number {
-    const distanceX = posX - this.$pointer[0].getBoundingClientRect().left;
-    const distanceY = posY - this.$pointer[0].getBoundingClientRect().top;
-    const distancePercentX = (distanceX / this.$pointer.parent().width()) * 100;
-    const distancePercentY = (distanceY / this.$pointer.parent().height()) * 100;
-    return (this.isVertical ? distancePercentY : distancePercentX);
-  }
-
-  private createDomElements(node: JQuery) {
-    this.$pointer = $('<div>', { class: 'slider__pointer' });
-    node.append(this.$pointer);
   }
 }
