90ff2dffb9855a9055a7e5635d09db59243e4729
exon <exon@exon.ru>
2021-11-30 11:11:12 +0500

    MovableObject class updated

--- src/view/moveableObject/MoveableObject.ts
+++ src/view/moveableObject/MoveableObject.ts
@@ -22,66 +22,61 @@ class MoveableObject implements MoveableObjectInterface {
     this.observer = observer;
   }
 
+  private handleObjectMouseMove = (event: JQuery.Event) => {
+    const distance = this.calcDistanceInPercent(
+      event.clientX - this.offsetX,
+      event.clientY - this.offsetY,
+    );
+    this.observer?.move(distance.x, distance.y);
+  };
+
+  private handleObjectMouseUp = () => {
+    $(document).off('mousemove', this.handleObjectMouseMove);
+    this.observer?.endMove();
+  };
+
+  private handleObjectMouseDown = (event: JQuery.Event) => {
+    $(document).on('mousemove', this.handleObjectMouseMove);
+    $(document).one('mouseup', this.handleObjectMouseUp);
+    this.offsetX = event.offsetX;
+    this.offsetY = event.offsetY;
+    this.observer?.startMove();
+  };
+
   private initMouseEvents() {
-    const mouseMoveHandler = function handleObjectMouseMove(event: MouseEvent) {
-      const distance = this.calcDistanceInPercent(
-        event.clientX - this.offsetX,
-        event.clientY - this.offsetY,
-      );
-      this.observer?.move(distance.x, distance.y);
-    }.bind(this);
-
-    const mouseUpHandler = function handleObjectMouseUp() {
-      $(document).off('mousemove', mouseMoveHandler);
-      this.observer?.endMove();
-    }.bind(this);
-
-    const mouseDownHandler = function handleObjectMouseDown(event: MouseEvent) {
-      $(document).on('mousemove', mouseMoveHandler);
-      $(document).one('mouseup', mouseUpHandler);
-      this.offsetX = event.offsetX;
-      this.offsetY = event.offsetY;
-      this.observer?.startMove();
-    }.bind(this);
-
-    this.$object.on('mousedown', mouseDownHandler);
+    this.$object.on('mousedown', this.handleObjectMouseDown);
   }
 
+  private handleObjectTouchMove = (event: TouchEvent) => {
+    const distance = this.calcDistanceInPercent(
+      event.touches[0].clientX,
+      event.touches[0].clientY,
+    );
+    this.observer?.move(distance.x, distance.y);
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  private handleObjectTouchEnd = () => {
+    document.removeEventListener('touchmove', this.handleObjectTouchMove);
+    this.observer?.endMove();
+  };
+
+  private handleObjectTouchStart = (event: TouchEvent) => {
+    document.addEventListener(
+      'touchmove', this.handleObjectTouchMove, { passive: false },
+    );
+    document.addEventListener(
+      'touchend', this.handleObjectTouchEnd, { once: true },
+    );
+    event.preventDefault();
+    event.stopPropagation();
+    this.observer?.startMove();
+  };
+
   private initTouchEvents() {
-    const touchMoveHandler = function handleObjectTouchMove(event: TouchEvent) {
-      const distance = this.calcDistanceInPercent(
-        event.touches[0].clientX,
-        event.touches[0].clientY,
-      );
-      this.observer?.move(distance.x, distance.y);
-      event.preventDefault();
-      event.stopPropagation();
-    }.bind(this);
-
-    const touchEndHandler = function handleObjectTouchEnd() {
-      document.removeEventListener('touchmove', touchMoveHandler);
-      document.removeEventListener('touchend', touchEndHandler);
-      this.observer?.endMove();
-    }.bind(this);
-
-    const touchStartHandler = function handleObjectTouchStart(
-      event: TouchEvent,
-    ) {
-      document.addEventListener(
-        'touchmove',
-        touchMoveHandler,
-        { passive: false },
-      );
-      document.addEventListener('touchend', touchEndHandler);
-      event.preventDefault();
-      event.stopPropagation();
-      this.observer?.startMove();
-    }.bind(this);
-
     this.$object[0].addEventListener(
-      'touchstart',
-      touchStartHandler,
-      { passive: false },
+      'touchstart', this.handleObjectTouchStart, { passive: false },
     );
   }
 
